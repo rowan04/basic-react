7044bbe739b92a0d96ff944951811eba2361227b
Rowan <rowanmoss04@gmail.com>
2024-05-10 17:02:04 +0100

    displaying data, conditions, lists, event response, updating

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,18 +1,66 @@
 import './App.css';
+import { useState } from 'react';
 
-function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
+const user = {
+  name: 'Rowan',
+  imageLoc: require('./images/pfp1.jpg'),
+  imageSize: 90
+};
+
+const cars = [
+  {title: 'Porsche', isBritish: false, id: 1},
+  {title: 'McLaren', isBritish: true, id: 2},
+  {title: 'Mercedes', isBritish: false, id: 3}
+]
+
 
 export default function MyApp() {
+
+  const listItems = cars.map(cars =>
+    <li
+      key={cars.id}
+      style={{
+        color: cars.isBritish ? 'blue' : 'red'
+      }}
+    >
+      {cars.title}
+    </li>
+  );
+
   return (
     <div>
       <h1>Welcome to my app</h1>
+      <h3>Counters that update seperately:</h3>
       <MyButton />
+      <br />
+      <MyButton />
+      <br />
+      <h1>{user.name}</h1>
+      <img src={user.imageLoc}
+        className='avatar'
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+      <br />
+      <ul>{listItems}</ul>
     </div>
+  );
+}
+
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
   );
 }
